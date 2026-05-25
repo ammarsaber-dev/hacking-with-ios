5f6fb4dc76466988a3c1694d0be1162afa92a6c7
ammarsaber-dev <ammarsaber.dev@icloud.com>
2026-05-25 10:28:19 +0300

    GuessTheFlag app completed

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Ammar Saber on 23/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
